1c21cef224b57b4017b1b71a668fe645e604fbc3
harokr <harokr@engr.orst.edu>
2022-03-08 10:19:55 -0800

    added esp32 code

---
+++ esp32 code/src/main.cpp
@@ -0,0 +1,107 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <TickTwo.h>
+#include <string.h>
+#include <time.h>
+
+void readNLog();
+
+const char* ssid     = "OnePlus 7T";
+const char* password = "d22f3117dc9e";
+
+const char*  server = "web.engr.oregonstate.edu";  // Server URL
+const int port = 443;
+String apiKeyValue = "tPmAT5Ab3j7F9"; // API key for communicating with serverName
+
+const char* ca_cert= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIF3jCCA8agAwIBAgIQAf1tMPyjylGoG7xkDjUDLTANBgkqhkiG9w0BAQwFADCB\n" \
+"iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl\n" \
+"cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV\n" \
+"BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTAw\n" \
+"MjAxMDAwMDAwWhcNMzgwMTE4MjM1OTU5WjCBiDELMAkGA1UEBhMCVVMxEzARBgNV\n" \
+"BAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0plcnNleSBDaXR5MR4wHAYDVQQKExVU\n" \
+"aGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNVBAMTJVVTRVJUcnVzdCBSU0EgQ2Vy\n" \
+"dGlmaWNhdGlvbiBBdXRob3JpdHkwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIK\n" \
+"AoICAQCAEmUXNg7D2wiz0KxXDXbtzSfTTK1Qg2HiqiBNCS1kCdzOiZ/MPans9s/B\n" \
+"3PHTsdZ7NygRK0faOca8Ohm0X6a9fZ2jY0K2dvKpOyuR+OJv0OwWIJAJPuLodMkY\n" \
+"tJHUYmTbf6MG8YgYapAiPLz+E/CHFHv25B+O1ORRxhFnRghRy4YUVD+8M/5+bJz/\n" \
+"Fp0YvVGONaanZshyZ9shZrHUm3gDwFA66Mzw3LyeTP6vBZY1H1dat//O+T23LLb2\n" \
+"VN3I5xI6Ta5MirdcmrS3ID3KfyI0rn47aGYBROcBTkZTmzNg95S+UzeQc0PzMsNT\n" \
+"79uq/nROacdrjGCT3sTHDN/hMq7MkztReJVni+49Vv4M0GkPGw/zJSZrM233bkf6\n" \
+"c0Plfg6lZrEpfDKEY1WJxA3Bk1QwGROs0303p+tdOmw1XNtB1xLaqUkL39iAigmT\n" \
+"Yo61Zs8liM2EuLE/pDkP2QKe6xJMlXzzawWpXhaDzLhn4ugTncxbgtNMs+1b/97l\n" \
+"c6wjOy0AvzVVdAlJ2ElYGn+SNuZRkg7zJn0cTRe8yexDJtC/QV9AqURE9JnnV4ee\n" \
+"UB9XVKg+/XRjL7FQZQnmWEIuQxpMtPAlR1n6BB6T1CZGSlCBst6+eLf8ZxXhyVeE\n" \
+"Hg9j1uliutZfVS7qXMYoCAQlObgOK6nyTJccBz8NUvXt7y+CDwIDAQABo0IwQDAd\n" \
+"BgNVHQ4EFgQUU3m/WqorSs9UgOHYm8Cd8rIDZsswDgYDVR0PAQH/BAQDAgEGMA8G\n" \
+"A1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQEMBQADggIBAFzUfA3P9wF9QZllDHPF\n" \
+"Up/L+M+ZBn8b2kMVn54CVVeWFPFSPCeHlCjtHzoBN6J2/FNQwISbxmtOuowhT6KO\n" \
+"VWKR82kV2LyI48SqC/3vqOlLVSoGIG1VeCkZ7l8wXEskEVX/JJpuXior7gtNn3/3\n" \
+"ATiUFJVDBwn7YKnuHKsSjKCaXqeYalltiz8I+8jRRa8YFWSQEg9zKC7F4iRO/Fjs\n" \
+"8PRF/iKz6y+O0tlFYQXBl2+odnKPi4w2r78NBc5xjeambx9spnFixdjQg3IM8WcR\n" \
+"iQycE0xyNN+81XHfqnHd4blsjDwSXWXavVcStkNr/+XeTWYRUc+ZruwXtuhxkYze\n" \
+"Sf7dNXGiFSeUHM9h4ya7b6NnJSFd5t0dCy5oGzuCr+yDZ4XUmFF0sbmZgIn/f3gZ\n" \
+"XHlKYC6SQK5MNyosycdiyA5d9zZbyuAlJQG03RoHnHcAP9Dc1ew91Pq7P8yF1m9/\n" \
+"qS3fuQL39ZeatTXaw2ewh0qpKJ4jjv9cJ2vhsE/zB+4ALtRZh8tSQZXq9EfX7mRB\n" \
+"VXyNWQKV3WKdwrnuWih0hKWbt5DHDAff9Yk2dDLWKMGwsAvgnEzDHNb842m1R0aB\n" \
+"L6KCq9NjRHDEjf8tM7qtj3u1cIiuPhnPQCjY/MiQu12ZIvVS5ljFH4gxQ+6IHdfG\n" \
+"jjxDah2nGN59PRbxYvnKkKj9\n" \
+"-----END CERTIFICATE-----\n";
+
+TickTwo timer1(readNLog, 1000*40); // once, every 40s
+WiFiClientSecure client;
+
+
+void setup() {
+  timer1.start();
+  Serial.begin(115200);
+   WiFi.mode(WIFI_STA); //The WiFi is in station mode. The    other is the softAP mode
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+   }
+   Serial.println("");  Serial.print("WiFi connected to: "); Serial.println(ssid);  Serial.println("IP address: ");  Serial.println(WiFi.localIP());
+   client.setCACert(ca_cert); //Only communicate with the server if the CA certificates match
+   delay(2000);
+}
+
+void loop() {
+ timer1.update();
+}
+
+void readNLog(){
+  int conn;
+  conn = client.connect(server, port);
+  String body = "api_key=" + apiKeyValue + "&btemp=" + String(24)
+                          + "&hr=" + String(69)
+                          + "&gsr=" + String(420) + "&switch=" + "0";
+  int body_len = body.length();
+  if (conn == 1) {
+      Serial.println(); Serial.print("Sending Parameters...");
+      //Request
+      client.println("POST /~harokr/post.php HTTP/1.1");
+      //Headers
+      client.print("Host: "); client.println(server);
+      client.println("Content-Type: application/x-www-form-urlencoded");
+      client.print("Content-Length: "); client.println(body_len);
+      client.println("Connection: Close");
+      client.println();
+      //Body
+      client.println(body);
+      client.println();
+
+      //Wait for server response
+      while (client.available() == 0);
+
+      //Print Server Response
+      while (client.available()) {
+         char c = client.read();
+         Serial.write(c);
+      }
+   } else {
+      client.stop();
+      Serial.println("Connection Failed");
+   }
+}
